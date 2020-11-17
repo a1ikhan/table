9ded4e6f00ec836102b654f45585a1409577a6c2
Alikhan Zhumazhanov <a_zhumazhanov@mail.ru>
2020-11-17 20:51:05 +0600

    first commit

---
+++ src/app/components/table/table-datasource.ts
@@ -0,0 +1,55 @@
+import {DataSource} from '@angular/cdk/collections';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {map} from 'rxjs/operators';
+import {Observable, of as observableOf, merge} from 'rxjs';
+import {EXAMPLE_DATA} from '../../data/someData';
+import {TableItem} from '../../interface/table';
+
+export class TableDataSource extends DataSource<TableItem> {
+      data: TableItem[] = EXAMPLE_DATA;
+      paginator: MatPaginator;
+      sort: MatSort;
+
+      constructor() {
+            super();
+      }
+
+      connect(): Observable<TableItem[]> {
+            const dataMutations = [
+                  observableOf(this.data),
+                  this.paginator.page,
+                  this.sort.sortChange
+            ];
+
+            return merge(...dataMutations).pipe(map(() => {
+                  return this.getPagedData(this.getSortedData([...this.data]));
+            }));
+      }
+
+      disconnect(): void {
+      }
+
+      private getPagedData(data: TableItem[]): any {
+            const startIndex = this.paginator.pageIndex * this.paginator.pageSize;
+            return data.splice(startIndex, this.paginator.pageSize);
+      }
+
+      private getSortedData(data: TableItem[]): any {
+            if (!this.sort.active || this.sort.direction === '') {
+                  return data;
+            }
+
+            return data.sort((a, b) => {
+                  const isAsc = this.sort.direction === 'asc';
+                  switch (this.sort.active) {
+                        default:
+                              return 0;
+                  }
+            });
+      }
+}
+
+function compare(a: string | number, b: string | number, isAsc: boolean): any {
+      return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+}
---
+++ src/app/interface/table.ts
@@ -0,0 +1,11 @@
+export interface TableItem {
+      orderNumber: string;
+      name: string;
+      company: string;
+      contractNumber: string;
+      analysis: string;
+      dateRegistration: Date;
+      dateFinish: Date;
+      laboratory: string;
+      status: string;
+}
